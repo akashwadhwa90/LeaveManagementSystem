29d4a33a5e8adbf0297d4554d1e07833463d6db9
Akash Wadhwa <akashwadhwa89@yahoo.com>
2024-09-01 17:56:39 +0530

    Implementing Service Layer

---
+++ LeaveManagementSystem.Web/MappingProfiles/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+
+namespace LeaveManagementSystem.Web.MappingProfiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<LeaveType, LeaveTypeReadOnlyVM>();
+            CreateMap<LeaveTypeCreateVM, LeaveType>();
+            CreateMap<LeaveTypeEditVM, LeaveType>().ReverseMap();
+
+        }
+    }
+}
